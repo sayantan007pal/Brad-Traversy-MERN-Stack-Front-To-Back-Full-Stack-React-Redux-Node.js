9b3b60937ab5fd2190d80ac7d425de1afc434735
sayantanpal <sayantanpal100@gmail.com>
2025-12-11 19:23:03 +0530

    feat: implement user registration with gravatar, password hashing, and database persistence for new users.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar")
+const bcrypt = require("bcryptjs")
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User")
 // @route    GET api/users
 // @desc     Get all users
 // @access   Public
@@ -9,10 +13,49 @@ router.post('/', [
     check("name", "Name is required").not().isEmpty(),
     check("email", "Please include a valid email").isEmail(),
     check("password", "please enter a password with 6 or more characters").isLength({min:6})
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
+    }
+
+const { name, email, password } = req.body;
+
+    try{
+    // see if user exists
+    let user = await User.findOne({email: email})
+    if(user){
+        return res.status(400).json({errors: [{msg: "User already exists"}]})
+    }
+    //get users' gravatar
+
+    const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+    })
+
+    user = new User({
+        name,
+        email,
+        avatar,
+        password
+    })
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt)
+
+    await user.save();
+
+    // encrypt password
+
+    // return json web token
+
+
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send("Server error")
+
     }
 
     
