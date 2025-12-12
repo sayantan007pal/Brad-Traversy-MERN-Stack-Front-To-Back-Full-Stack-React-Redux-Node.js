30e4a3a4c3fd1ecb1cb944f1f3084281faa8c44c
sayantanpal <sayantanpal100@gmail.com>
2025-12-12 23:37:29 +0530

    feat: implement user profile retrieval and create Profile model with schema

---
+++ models/Profile.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    },
+    company: {
+        type: String,
+    },
+    website: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true,
+    },
+    bio: {
+        type: String,
+
+    },
+    githubUserName: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Profile = mongoose.model("Profile", profileSchema);
