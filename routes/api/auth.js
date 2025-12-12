765fef14f02c183bbc19e802585a27df02581a76
sayantanpal <sayantanpal100@gmail.com>
2025-12-12 21:30:28 +0530

    feat: implement JWT authentication middleware for secure route access

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/User");
 
 // @route    GET api/auth
 // @desc     Get all auth
 // @access   Public
 
-router.get('/', (req, res) => {
-    res.send("Auth")
+router.get('/', auth, async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select("-password");
+        res.json(user);
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Internal Server Error");
+    }
 })
 
 module.exports = router
