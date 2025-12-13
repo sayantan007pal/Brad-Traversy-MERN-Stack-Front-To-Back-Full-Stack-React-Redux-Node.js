31799a494f8fab70cacc96d334f99d998abe2929
sayantanpal <sayantanpal100@gmail.com>
2025-12-13 18:50:16 +0530

    feat: implement registration form submission with axios integration

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
     const [formData, setFormData] = React.useState({
@@ -12,13 +13,32 @@ const Register = () => {
     const { name, email , password, password2 } = formData;
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
             console.log('Passwords do not match');
         } else {
-            console.log(formData);
-        }
+        //     const newUser = {
+        //         name,
+        //         email,
+        //         password
+        //     };
+        //     try {
+        //         const config ={
+        //             headers: {
+        //                 'Content-Type' : 'application/json'
+        //             }
+        //         }
+        //         const body = JSON.stringify(newUser);
+
+        //         const res = await axios.post('/api/users', body, config);
+        //         console.log(res.data);
+        //     } catch (err) {
+        //         console.error(err.response.data);
+
+                
+        //     }
+        // }
     };
 
     return (
